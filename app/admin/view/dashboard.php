734057b2f7b0e1dd096e2b88caa0e3fa162c9e47
italonascimento89 <italonascimento89@gmail.com>
2017-11-04 18:13:10 -0300

    Caminho das imagens do DashBoar e Usuarios desabilitados

--- app/admin/view/dashboard.php
+++ app/admin/view/dashboard.php
@@ -38,7 +38,7 @@
                     <a href="#">
                       <div class="pull-left">
                         <!-- User Image -->
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="app/assets/imgs/user9-640x640.jpg" class="img-circle" alt="User Image">
                       </div>
                       <!-- Message title and timestamp -->
                       <h4>
